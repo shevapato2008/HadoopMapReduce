ec1fe77a593097d63086defc843a5e285a0ca8e4
shevapato2008 <shevapato2008@gmail.com>
2016-12-31 04:45:04 -0500

    Added papers.

---
+++ 5. Word Count/WordFrequency.py
@@ -0,0 +1,23 @@
+from mrjob.job import MRJob
+
+class MRWordFrequencyCount(MRJob):
+
+    def mapper(self, _, line):
+        words = line.split()
+        for word in words:
+            word = unicode(word, "utf-8", errors = "ignore")    # avoids issues in mrjob 5.0
+            yield word.lower(), 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    MRWordFrequencyCount.run()
+
+'''
+import os
+os.chdir("..")
+cd 5. Word Count
+
+!python WordFrequency.py Book.txt > wordcount.txt
+'''
