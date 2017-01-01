5460e827460a02c1fbb2986ef401d09ce8632e09
shevapato2008 <shevapato2008@gmail.com>
2017-01-01 04:22:19 -0500

    Modified the code.

--- 6. Total Spent by Customer/CustomerSpentSorted.py
+++ 6. Total Spent by Customer/CustomerSpentSorted.py
@@ -1,13 +1,33 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
-class MRCustomerSpent(MRJob):
+class SpendByCustomerSorted(MRJob):
 
-    def mapper(self, _, line):
-        (customerID, itemID, spent) = line.split(',')
-        yield customerID, float(spent)
+    def steps(self):
+        return [
+            MRStep(mapper = self.mapper_getSpent,
+                   reducer = self.reducer_totalSpent),
+            MRStep(mapper = self.mapper_makeSpentKey,
+                   reducer = self.reducer_outputTopSpent)
+        ]
 
-    def reducer(self, customerID, spent):
-        yield customerID, sum(spent)
+    def mapper_getSpent(self, _, line):
+        (customerID, itemID, order) = line.split(',')
+        yield customerID, float(order)
+
+    def reducer_totalSpent(self, customerID, orders):
+        yield customerID, sum(orders)
+
+    def mapper_makeSpentKey(self, customerID, totalSpent):
+        yield '%04.02f'%float(totalSpent), customerID
+
+    def reducer_outputTopSpent(self, spent, customerIDList):
+        for customerID in customerIDList:
+            yield customerID, spent
 
 if __name__ == '__main__':
-    MRCustomerSpent.run();
+    SpendByCustomerSorted.run();
+
+'''
+!python CustomerSpentSorted.py customer-orders.csv > customerspentsorted.txt
+'''
